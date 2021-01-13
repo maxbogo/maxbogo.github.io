d367fb6511b6b4734b070c8815451fac2b3728ff
Jons Bori <jonsbori7@gmail.com>
2021-01-13 22:09:53 +0100

    check if the email written correct then show popup

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(() => {
 
     $("#mvp a").click(function(event) {
@@ -7,10 +5,11 @@ $(document).ready(() => {
         event.preventDefault();
 
     })
+
     $(".unterlagen-link").click(function(event) {
-        
+
         let subject = this.id.substr(2);
-    
+
         this.innerHTML = this.innerHTML === "Unterlagen anzeigen" ? "Unterlagen verstecken" : "Unterlagen anzeigen";
         $("#unterlagen_" + subject).toggle();
 
@@ -19,8 +18,38 @@ $(document).ready(() => {
     $("#email_text").keypress(function(e){
         if (e.key === 'Enter') {
             e.preventDefault();
-            $("#subscribe_modal").modal();
+            var email_written  = ($( "#email_text" ).val());
+            var email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+            if(email.test(email_written)){
+                $( "#subscribe_modal").css( {"display": "block"} );
+                $( "#email_error" ).css( {"display": "none"} );
+                $( "#email_text" ).val('');
+           }else{
+               $( "#email_error" ).css( {"display": "inline", "color": "#ca2400"} );
+           }
+            // $(#subscribe_modal").modal();
         }
     })
+
+    $("#close_button").click(function (e) {
+        $( "#subscribe_modal").css( {"display": "none"} );
+    });
+
+
+
+    $('#email_submit').click(function (e) {
+       e.preventDefault();
+       var email_written  = ($( "#email_text" ).val());
+       var email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+       if(email.test(email_written)){
+           $( "#subscribe_modal").css( {"display": "block"} );
+           $( "#email_error" ).css( {"display": "none"} );
+           $( "#email_text" ).val('');
+       }else{
+          $("#email_error" ).css( {"display": "inline", "color": "#ca2400"} );
+       }
+    });
+
+
 
 });
