55e7f57e9832c2a64ab66b4cef1e0b9938b96f88
julianschnee42 <julian.schnee@gmail.com>
2021-01-14 19:39:32 +0100

    added fake chat function to mvp

--- main.js
+++ main.js
@@ -12,11 +12,81 @@ $(document).ready(() => {
 
     };
 
+    const chat = {
+
+        pm: [
+
+            {time: "12.01 12:42", text: "Hat irgendwer eine Ahnung wie man mit Project Libre umgeht?", user: "Barbara Finkel"},
+            {time: "12.01 12:47", text: "Wenn du ein paar Minuten warten kannst kann ich dir ein schnelles Tutorial machen", user: "Katrin Schultheiss"},
+            {time: "12.01 12:48", text: "Das wäre super!!!", user: "Barbara Finkel"},
+            {time: "12.01 13:05", text: "Habs hochgeladen", user: "Katrin Schultheiss"},
+            {time: "12.01 13:07", text: "Danke du bist die beste!", user: "Barbara Finkel"}
+
+        ],
+
+        dbs: [
+
+            {time: "10.01 17:22", text: "Also ich hab mir grad eine Altklausuhr angesehen und gar nichts gecheckt. Kann mir da jemand helfen?", user: "Dieter Schulze"},
+            {time: "10.01 17:25", text: "Schau dir mal die Lösungen an die schon hochgeladen wurden, vielleicht hilft dir das ja", user: "Ralph Schroeder"},
+            {time: "10.01 17:28", text: "Ok gut das mach ich mal, danke für den Hinweis", user: "Dieter Schulze"},
+
+
+        ],
+        ids: [],
+        est: [],
+        num: []
+
+
+    }
+
     $("#mvp a").click(function(event) {
 
         event.preventDefault();
 
     })
+
+   function chatFactory(subject) {
+
+        if(Object.prototype.hasOwnProperty.call(chat, subject)) {
+
+            const td = $('<td colspan="4"></td>');
+
+            if(!chat[subject].length) {
+
+                td.text("Noch keine Nachrichten vorhanden...");
+                td.append($("<hr />"));
+
+            } else {
+
+                chat[subject].forEach(entry => {
+
+                    const time = $('<span class="chat-time"></span>');
+                    time.text(entry.time + " | ");
+    
+                    const user = $("<span></span");
+                    user.text(entry.user);
+    
+                    const msg = $('<div class="chat-content"></div>');
+                    msg.text(entry.text);
+    
+                    td.append(time);
+                    td.append(user);
+                    td.append(msg);
+                    td.append($("<hr />"));
+    
+                })
+
+            }
+
+            const button = $('<input type="text" class="form-control chat-user-input" placeholder="Deine Nachricht..." id="chat-user-input_' + subject + '" />');
+            td.append(button);
+            return td;
+
+        }
+
+    return $("<td></td>");
+
+    }
 
     function generateUlContent(subject) {
 
@@ -39,6 +109,8 @@ $(document).ready(() => {
 
     }
 
+
+
     $(".unterlagen-link").click(function(event) {
 
         let subject = this.id.substr(2);
@@ -46,6 +118,8 @@ $(document).ready(() => {
 
         this.innerHTML = this.innerHTML === "Unterlagen anzeigen" ? "Unterlagen verstecken" : "Unterlagen anzeigen";
         $("#unterlagen_" + subject).toggle();
+        $("#chat-tr_" + subject).hide();
+        $("#open-chat-link_" + subject).text("Chat öffnen");
 
     })
 
@@ -84,6 +158,61 @@ $(document).ready(() => {
         }
 
     })
+
+    $(".open-chat-link").click(function(e) {
+
+        let subject = this.id.substr(15);
+
+        const tr = $("#chat-tr_" + subject);
+
+        tr.empty();
+        tr.append(chatFactory(subject));
+
+        this.innerHTML = this.innerHTML === "Chat öffnen" ? "Chat schließen" : "Chat öffnen";
+        tr.toggle();
+        
+        $("#unterlagen_" + subject).hide();
+        $("#u_" + subject).text("Unterlagen anzeigen");
+
+    })
+
+    $(".chat-tr").on("keypress", ".chat-user-input", function(e) {
+
+        if(e.key === "Enter") {
+
+            let subject = this.id.substr(16);
+
+            let text = $(this).val();
+            if(!text) return;
+
+            let d = new Date();   
+            let time = d.getDate() + "." + fixMonth((d.getMonth() + 1)) + " " + d.getHours() + ":" + d.getMinutes();
+
+            chat[subject].push({
+
+                time: time,
+                text: text,
+                user: "Max Mustermann"
+
+            })
+
+            const tr = $("#chat-tr_" + subject);
+
+            tr.empty();
+            tr.append(chatFactory(subject));
+
+            $('#chat-user-input_' + subject).focus();
+
+        }
+
+    })
+
+    function fixMonth(month) {
+
+        if(month < 10) return "0" + month;
+        else return month;
+
+    }
 
 
     /* end MVP stuff */
